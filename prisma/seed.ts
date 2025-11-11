1dd452eed6f36d6b8b6f9296c87a1470d7f74a6a
Yuhang Wu <yuhang34@hawaii.edu>
2025-11-11 13:40:23 -1000

    Added default data for seeding database.
    Added default data for seeding database.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,14 @@
-import { PrismaClient, Role, Condition } from '@prisma/client';
+import {
+  PrismaClient,
+  Role,
+  Condition,
+  AccountPrivilege,
+  MerchStockStatus,
+  LengthUnit,
+  MassUnit,
+  MerchMaterial,
+  MerchCondition,
+} from '@prisma/client';
 import { hash } from 'bcrypt';
 import * as config from '../config/settings.development.json';
 
@@ -33,6 +43,53 @@ async function main() {
         quantity: data.quantity,
         owner: data.owner,
         condition,
+      },
+    });
+  }
+  for (const account of config.defaultAccount) {
+    const privilege = account.Privilege as AccountPrivilege || AccountPrivilege.USER;
+    // eslint-disable-next-line no-await-in-loop
+    await prisma.account.upsert({
+      where: { AccountID: config.defaultAccount.indexOf(account) + 1 },
+      update: {},
+      create: {
+        Privilege: privilege,
+        EmailAddress: account.EmailAddress,
+        Password: account.Password,
+        FirstName: account.FirstName,
+        MiddleName: account.MiddleName,
+        LastName: account.LastName,
+      } });
+  }
+  for (const merch of config.defaultMerch) {
+    const stock = merch.StockStatus as MerchStockStatus || MerchStockStatus.ON_STOCK;
+    const lengthUnit = merch.LUnit as LengthUnit || LengthUnit.CENTIMETER;
+    const widthUnit = merch.WUnit as LengthUnit || LengthUnit.CENTIMETER;
+    const heightUnit = merch.HUnit as LengthUnit || LengthUnit.CENTIMETER;
+    const massUnit = merch.MUnit as MassUnit || MassUnit.GRAM;
+    const material = merch.Material as MerchMaterial || MerchMaterial.ALUMINUM;
+    const condition = merch.Condition as MerchCondition || MerchCondition.NEW;
+    // eslint-disable-next-line no-await-in-loop
+    await prisma.merch.upsert({
+      where: { MerchID: config.defaultMerch.indexOf(merch) + 1 },
+      update: {},
+      create: {
+        AccountID: merch.AccountID,
+        StockStatus: stock,
+        Price: merch.Price,
+        Name: merch.Name,
+        Description: merch.Description,
+        Image: merch.Image,
+        Length: merch.Length,
+        Width: merch.Width,
+        Height: merch.Height,
+        Mass: merch.Mass,
+        LUnit: lengthUnit,
+        WUnit: widthUnit,
+        HUnit: heightUnit,
+        MUnit: massUnit,
+        Material: material,
+        Condition: condition,
       },
     });
   }
