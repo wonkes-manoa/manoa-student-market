171aa051f4f38cbb1a724095f61f62fb4771d531
Yuhang Wu <yuhang34@hawaii.edu>
2025-11-11 09:45:53 -1000

    Created the Merch Gallery component
    Created the preliminary Merch Gallery Component, which shows photographs for a merchandise.

---
+++ src/components/MerchGallery.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Card, Image } from 'react-bootstrap';
+
+const MerchGallery = ({ photograph }: { photograph : string[] }) => {
+  if (photograph.length === 0) {
+    photograph.push('');
+  }
+  const [selectedPhotographURL, setSelectedPhotographURL] = useState(photograph[0]);
+  return (
+    <Card>
+      <Card.Body>
+        <Image
+          src={selectedPhotographURL}
+          alt=""
+        />
+        {photograph.map((item) => (
+          <Image
+            key={item}
+            src={item}
+            onClick={() => setSelectedPhotographURL(item)}
+          />
+        ))}
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default MerchGallery;
