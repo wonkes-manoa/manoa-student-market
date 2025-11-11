91429d21bb795e8c2ea7f4c2db258bd8886b0107
Yuhang Wu <yuhang34@hawaii.edu>
2025-11-11 11:18:24 -1000

    Added the Merch Detail page.
    Added the Merch Detail page.

---
+++ src/app/merch-detail/page.tsx
@@ -0,0 +1,10 @@
+import MerchDetail from '@/components/MerchDetail';
+import { Container } from 'react-bootstrap';
+
+const MerchDetailPage = async () => (
+  <Container>
+    <MerchDetail />
+  </Container>
+);
+
+export default MerchDetailPage;
