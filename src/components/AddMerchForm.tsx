581a42719498d6cf8fc416762f53384bc14ab4f9
Yuhang Wu <yuhang34@hawaii.edu>
2025-11-15 00:00:03 -1000

    #23 Added image download feature
    Added a function for downloading images from database.

--- src/components/AddMerchForm.tsx
+++ src/components/AddMerchForm.tsx
@@ -50,21 +50,21 @@ const onSubmit = async (data: {
     Material: data.Material,
     Condition: data.Condition,
   });
-  const files = data.Image;
-  if (files && files.length > 0) {
+  const images = data.Image;
+  if (images && images.length > 0) {
     const uploadData = new FormData();
     uploadData.append('MerchID', String(newMerch.MerchID));
 
-    for (const file of files) {
-      uploadData.append('Image', file);
+    for (const image of images) {
+      uploadData.append('Image', image);
     }
 
-    const res = await fetch('/api/upload/merch-images', {
+    const result = await fetch('/api/upload/merch-images', {
       method: 'POST',
       body: uploadData,
     });
 
-    if (!res.ok) {
+    if (!result.ok) {
       console.error('Image upload failed');
     }
   }
