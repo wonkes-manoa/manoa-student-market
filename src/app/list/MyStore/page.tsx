2cb4233774a3c02dd7dc81fe695b8467bc3ca094
Joshua Omori <oomorijosh@gmail.com>
2025-11-17 11:09:46 -1000

    ES Lint fix.

--- src/app/list/MyStore/page.tsx
+++ src/app/list/MyStore/page.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 /** The user list page. */
 const MyStore = () => (
@@ -7,13 +7,15 @@ const MyStore = () => (
       <Row className="align-middle text-center">
         <Col xs={8} className="d-flex flex-column justify-content-center">
           <h1>Your Listings</h1>
-        </Col> 
+        </Col>
         <Col><p>Filter By: Open Listings | Archived</p></Col>
       </Row>
-       <Row></Row> {/* Empty row for spacing */}
+      <Row />
+      {' '}
+      {/* Empty row for spacing */}
       <Row className="align-middle text-center">
 
-      {/* 3x# Grid of user listings to go here | use ListingCard.tsx component */}
+        {/* 3x# Grid of user listings to go here | use ListingCard.tsx component */}
       </Row>
 
     </Container>
