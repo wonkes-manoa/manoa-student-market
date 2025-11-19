8ef23ebf8ab3b90d11ec58a57c7b711e54383108
Andrew <narcisoandrewp@gmail.com>
2025-11-19 03:10:08 -1000

    linting

--- src/app/list/MyStore/page.tsx
+++ src/app/list/MyStore/page.tsx
@@ -14,35 +14,40 @@ const MyStorePage = async () => (
       <Col>
         <h5>Filter by:</h5>
         <Container className="btn-group">
-          <a href="/" className="btn btn-primary">Open Listings</a>
-          <a href="/" className="btn btn-default">Archived</a>
+          <a href="/" className="btn btn-primary">
+            Open Listings
+          </a>
+          <a href="/" className="btn btn-default">
+            Archived
+          </a>
         </Container>
-
       </Col>
-      
     </Row>
-    <Row />
-    {' '}
-    {/* Empty row for spacing */}
-              <div className="pb-5">
-            <Row className="g-3">
-              {Array.from({ length: 9 }).map((_, idx) => (
-                <Col key={idx} md={4} sm={6} xs={12}>
-                  <div
-                    className="bg-light rounded text-black p-2 d-flex flex-column"
-                    style={{ minHeight: '140px', maxWidth: '100%' }}
-                  >
-                    <h5 className="text-center mb-2">Box {idx + 1}</h5>
-                    <p className="flex-grow-1 small mb-2">Placeholder content for box {idx + 1}.</p>
-                    <Button size="sm" className="d-block mx-auto bg-success text-black">Learn More</Button>
-                  </div>
-                </Col>
-              ))}
-            </Row>
-          </div>
+    <Row /> {/* Empty row for spacing */}
+    <div className="pb-5">
+      <Row className="g-3">
+        {Array.from({ length: 9 }).map((_, idx) => (
+          <Col key={idx} md={4} sm={6} xs={12}>
+            <div
+              className="bg-light rounded text-black p-2 d-flex flex-column"
+              style={{ minHeight: '140px', maxWidth: '100%' }}
+            >
+              <h5 className="text-center mb-2">Box {idx + 1}</h5>
+              <p className="flex-grow-1 small mb-2">
+                Placeholder content for box {idx + 1}.
+              </p>
+              <Button
+                size="sm"
+                className="d-block mx-auto bg-success text-black"
+              >
+                Learn More
+              </Button>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </div>
   </Container>
-
-
 
   // card usage when a database is present
   // noStore(); // Ensure nothing cached, and all data fetch from database upon every request.
@@ -84,6 +89,6 @@ const MyStorePage = async () => (
   //     </Row>
   //   </Container>
   // );
-            );
+);
 
 export default MyStorePage;
