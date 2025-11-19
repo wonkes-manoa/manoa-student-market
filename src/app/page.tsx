559ac976ec96610ae1a5429ebec6adf697d41eb6
Joshua Omori <oomorijosh@gmail.com>
2025-11-18 22:02:12 -1000

    Finished Landing Page Design
    All that is left is to replace placeholder images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import LandingPage from '@/components/LandingPage';
 
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col lg={3} className="d-flex justify-content-center mb-3 mb-lg-0">
-          <h1>Welcome to Wonkes!</h1>
-        </Col>
-      </Row>
-    </Container>
+    <LandingPage />
   </main>
 );
 
