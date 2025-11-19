52b4c1fbd640c596a7aa20b8bd8926e3c8d8d417
Joshua Omori <oomorijosh@gmail.com>
2025-11-19 09:25:56 -1000

    Update signup redirect and remove unused Prisma models
    Changed the signup page redirect from '/add' to '/listings-view'. Removed unused User and Stuff models, as well as related enums, from the Prisma schema to clean up the database structure.

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -71,7 +71,7 @@ const SignUp = () => {
     await signIn('credentials', {
       username: data.username,
       password: data.password,
-      callbackUrl: '/add',
+      callbackUrl: '/listings-view',
     });
   };
 
