1bc0395bc7f017dc96f73703c184201d1bf17bea
Yuhang Wu <yuhang34@hawaii.edu>
2025-12-01 10:37:55 -1000

    Fixed naming error of GuestOnly component

--- src/components/GuestOnly.tsx
+++ src/components/GuestOnly.tsx
@@ -3,7 +3,7 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
-export default function AuthOnly({ children }: { children: React.ReactNode }) {
+export default function GuestOnly({ children }: { children: React.ReactNode }) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data: session, status } = useSession();
   const router = useRouter();
