f733fe7c2ac0ca87bed81377b496052b23f21aeb
darilyne <evangelistadarilyn@gmail.com>
2025-12-09 11:33:11 -1000

    fixed eslint errors

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -97,75 +97,75 @@ const SignUp = () => {
                   <h1 className="text-center">Sign Up</h1>
                   <Form onSubmit={handleSubmit(onSubmit)}>
                     <Row>
-                    {/* Username */}
-                    <Col md={6}>
-                    <Form.Group className="form-group">
-                      <Form.Label>Username</Form.Label>
-                      <input
-                        type="text"
-                        {...register('username')}
-                        className={`form-control ${errors.username ? 'is-invalid' : ''}`}
-                      />
-                      <div className="invalid-feedback">{errors.username?.message}</div>
-                    </Form.Group>
+                      {/* Username */}
+                      <Col md={6}>
+                        <Form.Group className="form-group">
+                          <Form.Label>Username</Form.Label>
+                          <input
+                            type="text"
+                            {...register('username')}
+                            className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+                          />
+                          <div className="invalid-feedback">{errors.username?.message}</div>
+                        </Form.Group>
 
-                    {/* Email */}
-                    <Form.Group className="form-group">
-                      <Form.Label>Email Address</Form.Label>
-                      <input
-                        type="text"
-                        {...register('email')}
-                        className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-                      />
-                      <div className="invalid-feedback">{errors.email?.message}</div>
-                    </Form.Group>
+                        {/* Email */}
+                        <Form.Group className="form-group">
+                          <Form.Label>Email Address</Form.Label>
+                          <input
+                            type="text"
+                            {...register('email')}
+                            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                          />
+                          <div className="invalid-feedback">{errors.email?.message}</div>
+                        </Form.Group>
 
-                    {/* First Name */}
-                    <Form.Group className="form-group">
-                      <Form.Label>First Name</Form.Label>
-                      <input
-                        type="text"
-                        {...register('firstName')}
-                        className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
-                      />
-                      <div className="invalid-feedback">{errors.firstName?.message}</div>
-                    </Form.Group>
-                    </Col>
+                        {/* First Name */}
+                        <Form.Group className="form-group">
+                          <Form.Label>First Name</Form.Label>
+                          <input
+                            type="text"
+                            {...register('firstName')}
+                            className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
+                          />
+                          <div className="invalid-feedback">{errors.firstName?.message}</div>
+                        </Form.Group>
+                      </Col>
 
-                    <Col md={6}>
-                    {/* Last Name */}
-                    <Form.Group className="form-group">
-                      <Form.Label>Last Name</Form.Label>
-                      <input
-                        type="text"
-                        {...register('lastName')}
-                        className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
-                      />
-                      <div className="invalid-feedback">{errors.lastName?.message}</div>
-                    </Form.Group>
+                      <Col md={6}>
+                        {/* Last Name */}
+                        <Form.Group className="form-group">
+                          <Form.Label>Last Name</Form.Label>
+                          <input
+                            type="text"
+                            {...register('lastName')}
+                            className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
+                          />
+                          <div className="invalid-feedback">{errors.lastName?.message}</div>
+                        </Form.Group>
 
-                    {/* Password */}
-                    <Form.Group className="form-group">
-                      <Form.Label>Password</Form.Label>
-                      <input
-                        type="password"
-                        {...register('password')}
-                        className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-                      />
-                      <div className="invalid-feedback">{errors.password?.message}</div>
-                    </Form.Group>
+                        {/* Password */}
+                        <Form.Group className="form-group">
+                          <Form.Label>Password</Form.Label>
+                          <input
+                            type="password"
+                            {...register('password')}
+                            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                          />
+                          <div className="invalid-feedback">{errors.password?.message}</div>
+                        </Form.Group>
 
-                    {/* Confirm Password */}
-                    <Form.Group className="form-group">
-                      <Form.Label>Confirm Password</Form.Label>
-                      <input
-                        type="password"
-                        {...register('confirmPassword')}
-                        className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}
-                      />
-                      <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
-                    </Form.Group>
-                    </Col>
+                        {/* Confirm Password */}
+                        <Form.Group className="form-group">
+                          <Form.Label>Confirm Password</Form.Label>
+                          <input
+                            type="password"
+                            {...register('confirmPassword')}
+                            className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}
+                          />
+                          <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
+                        </Form.Group>
+                      </Col>
                     </Row>
 
                     {/* Submit Button */}
