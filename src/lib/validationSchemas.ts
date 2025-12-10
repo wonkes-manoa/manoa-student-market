4d76ce78dc642ddd663de14703333553dd914afb
Yuhang Wu <yuhang34@hawaii.edu>
2025-12-10 12:33:46 -1000

    #102 Deleted unused validation schema

--- src/lib/validationSchemas.ts
+++ src/lib/validationSchemas.ts
@@ -1,21 +1,6 @@
 import { LengthUnit, MassUnit, MerchCondition, MerchMaterial, MerchStockStatus } from '@prisma/client';
 import * as Yup from 'yup';
 
-export const AddStuffSchema = Yup.object({
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
-
-export const EditStuffSchema = Yup.object({
-  id: Yup.number().required(),
-  name: Yup.string().required(),
-  quantity: Yup.number().positive().required(),
-  condition: Yup.string().oneOf(['excellent', 'good', 'fair', 'poor']).required(),
-  owner: Yup.string().required(),
-});
-
 export const AddMerchSchema = Yup.object({
   AccountID: Yup.number()
     .required(),
